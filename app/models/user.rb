277de9645eaf5529e5b8af524fee45acc24a6ab8
tokyosuite <helenyueyu@gmail.com>
2020-05-12 11:00:16 -0700

    restarting bug_mojo project

---
+++ app/models/user.rb
@@ -0,0 +1,42 @@
+class User < ApplicationRecord
+    attr_reader :password 
+
+    validates :username, :email, :password_digest, :session_token, presence: true 
+    validates :username, :email, :session_token, uniqueness: true 
+    validates :password, length: { minimum: 8 }, format: {with: /\A(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)\z/, message: 'must have 1 letter and 1 number'}, allow_nil: true 
+
+    after_initialize :ensure_session_token, :ensure_username  
+
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email) 
+        user && user.is_password?(password) ? user : nil 
+    end 
+
+    def password=(password)
+        @password = password 
+        self.password_digest = BCrypt::Password.create(password)
+    end 
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom.urlsafe_base64
+        self.save! 
+        self.session_token 
+    end
+
+    def ensure_session_token 
+        self.session_token ||= SecureRandom.urlsafe_base64
+    end
+
+    # going to shorten this to 5 digits but it might be not unique for very large userbase
+    def ensure_username
+        if self.username != ""
+            self.username 
+        else
+            self.username = "user#{((self.email).hash).to_s[0,6]}"
+        end
+    end
+end
