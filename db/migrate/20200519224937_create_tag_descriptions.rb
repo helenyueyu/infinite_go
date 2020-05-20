cf2e9bdbdf39f62b5cb2da93350ab9af001fb7d0
tokyosuite <helenyueyu@gmail.com>
2020-05-19 18:01:19 -0700

    add splash

--- db/migrate/20200519224937_create_tag_descriptions.rb
+++ db/migrate/20200519224937_create_tag_descriptions.rb
@@ -1,8 +1,12 @@
 class CreateTagDescriptions < ActiveRecord::Migration[5.2]
   def change
     create_table :tag_descriptions do |t|
+      t.text :description, null: false 
+      t.integer :tag_id, null: false 
 
       t.timestamps
     end
+
+    add_index :tag_descriptions, :tag_id, unique: true 
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_18_034227) do
+ActiveRecord::Schema.define(version: 2020_05_19_224937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,14 @@ ActiveRecord::Schema.define(version: 2020_05_18_034227) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_questions_on_user_id"
+  end
+
+  create_table "tag_descriptions", force: :cascade do |t|
+    t.text "description", null: false
+    t.integer "tag_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tag_id"], name: "index_tag_descriptions_on_tag_id", unique: true
   end
 
   create_table "tags", force: :cascade do |t|
