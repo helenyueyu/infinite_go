941972140302c32ff0cebc47e3ea7d4f9f2b9283
Helen Yu <helenyueyu@gmail.com>
2020-06-13 14:06:20 -0700

    add styling to comment and reformat date for comment

--- frontend/components/comment/comments.jsx
+++ frontend/components/comment/comments.jsx
@@ -1,11 +1,19 @@
 import React from 'react'; 
+import { displayDate } from '../../selectors/date_selectors'; 
 
 class Comments extends React.Component {
     render() {
         let { comments } = this.props; 
+        console.log('comments', comments); 
         return (
-            <div>
-                {Object.values(comments).map((x, idx) => <div key={idx}>{x.body}{x.username}</div>)}
+            <div className="comments">
+                {Object.values(comments).map((x, idx) => 
+                    <div key={idx} className="comments-item">
+                        <span className="comments-body">{x.body}</span>
+                        <span className="comments-dash">&mdash;</span>
+                        <span className="comments-username">{x.username}</span>
+                        <span className="comments-date">{displayDate(x.createdAt)}</span>
+                    </div>)}
             </div>
         )
     }
---
+++ frontend/selectors/date_selectors.js
@@ -0,0 +1,30 @@
+// 2020-06-13T20:33:01.057Z
+
+const monthMap = {
+    '01': 'Jan', 
+    '02': 'Feb', 
+    '03': 'Mar', 
+    '04': 'Apr', 
+    '05': 'May', 
+    '06': 'Jun', 
+    '07': 'Jul', 
+    '08': 'Aug', 
+    '09': 'Sept', 
+    '10': 'Oct', 
+    '11': 'Nov', 
+    '12': 'Dec'
+}
+
+export const displayDate = (date) => {
+    const chunks = date.split('-'); 
+    chunks[2] = chunks[2].slice(0, 2); 
+
+    const times = date.split(':'); 
+    times[0] = times[0].slice(times[0].length-2, times[0].length); 
+    times[2] = times[2].slice(0, 2); 
+
+    const [hours, minutes, seconds] = times; 
+
+    const [year, month, day] = chunks; 
+    return `${monthMap[month]} ${day} '${year.slice(2)} at ${hours}:${minutes}`; 
+}
