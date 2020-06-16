458588c7d058f78ed7ff0f93c5654810c5264614
Helen Yu <helenyueyu@gmail.com>
2020-06-16 10:53:19 -0700

    fix gold, silver, bronze medal display in user activity page

--- frontend/components/buttons/profile_buttons.jsx
+++ frontend/components/buttons/profile_buttons.jsx
@@ -5,18 +5,17 @@ import { Link } from 'react-router-dom';
 const ProfileButtons = ({id, activeIdx }) => {
     return (
         <div>
-            
-                <Link to={`/users/${id}`}>
-                    <button className={activeIdx === 0 ? "profile-button-active" : "profile-button"}>
-                        Profile
-                    </button>
-                </Link>
-            
-                <Link to={`/users/${id}/activity`}>
-                    <button className={activeIdx === 1 ? "profile-button-active" : "profile-button"}>
-                        Activity
-                    </button>
-                </Link>
+            <Link to={`/users/${id}`}>
+                <button className={activeIdx === 0 ? "profile-button-active" : "profile-button"}>
+                    Profile
+                </button>
+            </Link>
+        
+            <Link to={`/users/${id}/activity`}>
+                <button className={activeIdx === 1 ? "profile-button-active" : "profile-button"}>
+                    Activity
+                </button>
+            </Link>
             
             <button className="profile-button">Developer Story</button>
         </div>
--- frontend/components/user/profile/profile_activity.jsx
+++ frontend/components/user/profile/profile_activity.jsx
@@ -8,14 +8,40 @@ class ProfileActivity extends React.Component {
     }
 
     render() {
-        if (this.props.users[this.props.match.params.userId] === undefined) {
+        let user = this.props.users[this.props.match.params.userId]; 
+        if (user === undefined) {
             return null;
         } 
-        let { id } = this.props.users[this.props.match.params.userId]; 
+
+        let { id, medals } = user;  
+        console.log('user', user); 
         return (
             <div className="profile_activity">
                 <ProfileButtons id={id}
                         activeIdx={1} />
+                <div className="profile_activity-boxes">
+                    <div className="profile_activity-box_1">
+                        <div className="activity_box_title">Reputation</div>
+                    </div>
+                    <div className="profile_activity-box_2">
+                        <div className="activity_box_title">Badges</div>
+                        <div className="profile_top-medals">
+                            {medals.map((medal, idx) =>
+                                <div key={idx} className={idx === 0 ? "profile_top-medal-gold" : idx === 1 ? "profile_top-medal-silver" : "profile_top-medal-bronze"}>
+                                    <div>
+                                        &bull;<span className="profile_top-medal-count">{medal}</span>
+                                    </div>
+                                </div>)}
+                        </div>
+                    </div>
+                    <div className="profile_activity-box_3">
+                        <div className="activity_box_title">Impact</div>
+                    </div>
+                </div>
+
+                <div>
+                    
+                </div>
             </div>
         )
     }
