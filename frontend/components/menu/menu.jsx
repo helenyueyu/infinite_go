13f1fafba5ef01e3c44fae0151509d9608ef5c4f
Helen Yu <helenyueyu@gmail.com>
2020-06-20 12:40:57 -0700

    refactor active path to be if route starts with '/questions' rather than being identical to '/questions' (doesn't seem to work though...)

--- frontend/components/menu/menu.jsx
+++ frontend/components/menu/menu.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 class Menu extends React.Component {
     render() {
         const { location: {pathname} } = this.props; 
+
         return (
             <ul className="menu">
                 <li className={pathname === "/" ? "menu-item-link-active" : "menu-item-link"}>
@@ -12,7 +13,7 @@ class Menu extends React.Component {
                 <li className="menu-public">PUBLIC</li>
        
                 <li className={pathname === "/questions" ? "menu-item-link-active" : "menu-item-link"}>
-                    <i className={pathname === "/questions" ? "fas fa-globe-americas active" : "fas fa-globe-americas"}></i> 
+                    <i className={pathname.startsWith("/questions") ? "fas fa-globe-americas active" : "fas fa-globe-americas"}></i> 
                     <Link className="menu-item-link" to='/questions'>Infinite Go</Link>
                 </li>
                 <li className={pathname === "/users" ? "menu-item-link-active" : "menu-item-link"}>
