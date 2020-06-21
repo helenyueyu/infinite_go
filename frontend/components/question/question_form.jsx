ca0b944f27f2199f54a7a2000e6de4a5029528cc
Helen Yu <helenyueyu@gmail.com>
2020-06-21 10:30:35 -0700

    style tags in question form to be close to SO look

--- frontend/components/question/question_form.jsx
+++ frontend/components/question/question_form.jsx
@@ -282,7 +282,7 @@ class QuestionForm extends React.Component {
                 <div className="question_form-title-desc">
                     Add up to 5 tags to describe what your question is about                
                 </div>
-                <div className="tags">
+                <div className="question_form-tags">
                     {this.state.tags.map((tag, idx) => 
                         <div key={idx} className="question_form-tag-item">
                             {tag}
