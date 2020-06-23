0d73f1717649b9db95db30817ecdff0a749f80ad
Helen Yu <helenyueyu@gmail.com>
2020-06-23 10:56:46 -0700

    add styling to watched_tags box

--- frontend/components/menu/watched_tags.jsx
+++ frontend/components/menu/watched_tags.jsx
@@ -1,25 +1,43 @@
 import React from 'react'; 
 
 class WatchedTags extends React.Component {
+    constructor(props) {
+        super(props); 
+        this.state = {
+            watchedTagFormShown: false 
+        }
+    }
     render() {
         const { tags, watchedTags, userId, addToWatchTags, handleChange } = this.props; 
-        // debugger; 
+        const { watchedTagFormShown } = this.state;  
+
         return (
-            <div>
-                <input className="tags_index-search"
+            <div className="watched_tags">
+                <div className="watched_tags-title">
+                    <i className="fas fa-eye"></i>
+                    <span className="watched_tags-title-text">
+                        Watched Tags
+                    </span>
+                </div>
+                {watchedTagFormShown ? <input className="tags_index-search"
                     onChange={(e) => handleChange(e)}>
-                </input>
+                </input> : null}
 
                 <div>
                     {Object.keys(tags).length > 0 ? tags.slice(0, 5).map((tag, idx) => 
                         <div key={idx} 
-                            onClick={() => addToWatchTags(tag.id, userId)}>
+                            onClick={() => addToWatchTags(tag.id, userId)}
+                            >
                             {tag.name}
                         </div>) : 
                         null}
                 </div>
-                <div>
-                    {watchedTags.map((tag, idx) => <div key={idx}>{tag.name}</div>)}
+                <div className="watched_tags-items">
+                    {watchedTags.map((tag, idx) => 
+                        <div key={idx}
+                            className="watched_tags-item">
+                            {tag.name}
+                        </div>)}
                 </div>
             </div>
         )
