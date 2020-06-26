89ad1a20f37e9d9d87e2ae390286b26f370fe78e
Helen Yu <helenyueyu@gmail.com>
2020-06-26 13:51:19 -0700

    edit left sidebar active/or not depending if the url matches

--- frontend/components/menu/menu.jsx
+++ frontend/components/menu/menu.jsx
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 class Menu extends React.Component {
     render() {
         const { location: {pathname} } = this.props; 
-
         return (
             <ul className="menu">
                 <li className={pathname === "/" ? "menu-item-link-active" : "menu-item-link"}>
@@ -12,17 +11,17 @@ class Menu extends React.Component {
                 </li>
                 <li className="menu-public">PUBLIC</li>
        
-                <li className={pathname === "/questions" ? "menu-item-link-active" : "menu-item-link"}>
+                <li className={pathname.startsWith("/questions") ? "menu-item-link-active" : "menu-item-link"}>
                     <i className={pathname.startsWith("/questions") ? "fas fa-globe-americas active" : "fas fa-globe-americas"}></i> 
                     <Link className="menu-item-link" to='/questions'>Infinite Go</Link>
                 </li>
-                <li className={pathname === "/users" ? "menu-item-link-active" : "menu-item-link"}>
+                <li className={pathname.startsWith("/users") ? "menu-item-link-active" : "menu-item-link"}>
                     <Link className="menu-item-link types" to='/users'>Users</Link>
                 </li>
-                <li className={pathname === "/tags" ? "menu-item-link-active" : "menu-item-link"}>
+                <li className={pathname.startsWith("/tags") ? "menu-item-link-active" : "menu-item-link"}>
                     <Link className="menu-item-link types" to='/tags'>Tags</Link>
                 </li>
-                <li className={pathname === "/jobs" ? "menu-item-link-active" : "menu-item-link"}>
+                <li className={pathname.startsWith("/jobs") ? "menu-item-link-active" : "menu-item-link"}>
                     <Link className="menu-item-link types" to='/jobs'>Jobs</Link>
                 </li>
             </ul>
